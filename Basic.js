e892bb026cfd8e608e520bf6b71d7bf7d0ffc619
Balaji1416 <balajivel1416@gmail.com>
2025-09-23 14:52:49 +0530

    changes in basic.js

--- Basic.js
+++ Basic.js
@@ -1,3 +1,23 @@
-var num = "Java script";
+// var function is you can rename, reasign, using same name and it is old method
+num = "Welcome";
 var num = "Hello js";
-num = "Welcome"
+var num = "Java script";
+console.log(num);
+
+
+// let function 
+// let function is you asign and resign 
+
+let hello = 25;
+// let hello = 56; // we cannot use the same keyword
+hello = 66;
+
+console.log(hello);
+
+//const function 
+// const is a name like const func and once set the name we cannot change
+const arr = [2,63,42,54,646,6,68,];
+// const arr = [12,333,64,25,64,3,342]; cannot using same name
+// arr = [1,1,45,54,3,2,25]; and this also cannot change
+
+console.log(arr);
