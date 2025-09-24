de5c1e8c8eabdc597215452343f2be3c3821900a
Balaji1416 <balajivel1416@gmail.com>
2025-09-24 13:27:39 +0530

    add assingmnetoperator.js

--- assignmentoperator.js
+++ assignmentoperator.js
@@ -13,15 +13,15 @@
 
 // //Relational operators
 
-// console.log(10 < 20); //less than
-// console.log(10 < 9); //is not grater than 10
-// console.log(20 > 6); //Greater than
-// console.log(20 >= 20); //Greater than or equal to
-// console.log(40 == "40"); //double equal
-// console.log(40 === 40); //strict equal
-// console.log(40 != 50); //not equal
-// console.log(40 !== 50); //strict not equal
-// console.log(40 !== "57"); //strict not equal
+console.log(10 < 20); //less than
+console.log(10 < 9); //is not grater than 10
+console.log(20 > 6); //Greater than
+console.log(20 >= 20); //Greater than or equal to
+console.log(40 == "40"); //double equal
+console.log(40 === 40); //strict equal
+console.log(40 != 50); //not equal
+console.log(40 !== 50); //strict not equal
+console.log(40 !== "57"); //strict not equal
 
 //logical operators
 
@@ -32,14 +32,14 @@
    false   true    false
    false   false   false*/
 
-// const age = 18;
-// console.log(age >=18 && age <=30); //true and true
-// const age1 = 35;
-// console.log(age1 >=18 && age1 <=30); //true and false
-// const age2 = 16;
-// console.log(age2 >=18 && age2 <=30); //false and true // short circuit evaluation(the first condition would be false they never go to the next condition)
-// const idproof = "aadhar";
-// console.log(idproof == "pancard" && idproof == "ration card"); //false and false // short circuit evaluation
+const age = 18;
+console.log(age >=18 && age <=30); //true and true
+const age1 = 35;
+console.log(age1 >=18 && age1 <=30); //true and false
+const age2 = 16;
+console.log(age2 >=18 && age2 <=30); //false and true // short circuit evaluation(the first condition would be false they never go to the next condition)
+const idproof = "aadhar";
+console.log(idproof == "pancard" && idproof == "ration card"); //false and false // short circuit evaluation
 
 //logical OR (||) (any one condition true they true)
 /* cind1  cond2  result
